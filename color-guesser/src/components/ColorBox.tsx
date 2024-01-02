6397b0e5f4b8e04b6672333e4e0f1f8dc010741e
zanehirning <a02371568@usu.edu>
2024-01-01 20:43:03 -0700

    ignore node_modules, disable button so they cannot click wrong answer again

--- color-guesser/src/components/ColorBox.tsx
+++ color-guesser/src/components/ColorBox.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'
+import { BaseSyntheticEvent, useEffect, useState } from 'react'
 
 const getRandomColor = () => {
-  const possibleValues:string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-  const color = new Array(6).fill('').map(() => possibleValues[Math.floor(Math.random()*possibleValues.length)])
+  const possibleValues: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  const color = new Array(6).fill('').map(() => possibleValues[Math.floor(Math.random() * possibleValues.length)])
   return `#${color.join("")}`
 }
 
@@ -20,28 +20,29 @@ export default function ColorBox() {
 
   const generateColor = () => {
     const actualColor = getRandomColor()
-    setOptions([actualColor, getRandomColor(), getRandomColor(), getRandomColor()].sort(() => 0.5-Math.random()))
+    setOptions([actualColor, getRandomColor(), getRandomColor(), getRandomColor()].sort(() => 0.5 - Math.random()))
     setColor(actualColor)
   }
-  
+
   useEffect(() => {
     generateColor();
   }, [])
 
-  function handleAnswerClicked(answer:string) {
+  function handleAnswerClicked(e: BaseSyntheticEvent, answer: string) {
     if (answer === color) {
-        setIsResult(Result.Correct);
-        generateColor();
+      setIsResult(Result.Correct);
+      generateColor();
     } else {
-        setIsResult(Result.Wrong);
+      e.target.disabled = true;
+      setIsResult(Result.Wrong);
     }
   }
 
   return (
     <div className='wrapper-flex'>
-      {result === Result.Wrong && <div style={{color: "red", fontSize: "1.5rem", fontStyle: "system-ui"}}> Wrong Answer </div>}
-      {result === Result.Correct && <div style={{color: "green", fontSize: "1.5rem", fontStyle: "system-ui"}}>Correct</div>}
-      <div 
+      {result === Result.Wrong && <div style={{ color: "red", fontSize: "1.5rem", fontStyle: "system-ui" }}> Wrong Answer </div>}
+      {result === Result.Correct && <div style={{ color: "green", fontSize: "1.5rem", fontStyle: "system-ui" }}>Correct</div>}
+      <div
         className='color-box'
         style={{
           backgroundColor: `${color}`,
@@ -49,11 +50,11 @@ export default function ColorBox() {
       >
       </div>
       <span>
-          {options.map((option) => (
-            <button onClick={() => handleAnswerClicked(option)} key={option}>
-                  {option}
-            </button>
-          ))}
+        {options.map((option) => (
+          <button onClick={(e) => handleAnswerClicked(e, option)} key={option}>
+            {option}
+          </button>
+        ))}
       </span>
     </div>
   )
